964415e417761f2d401ae015e0672e18d633ffcf
ayuljain7 <ayuljain7@gmail.com>
2020-07-01 23:20:08 +0530

    Day 1

---
+++ Day 1/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int i=1;
+        int count=0;
+        while(i<=n)
+        {
+            n=n-i;
+            i++;
+            count++;
+        }
+        return count;
+    }
+};
